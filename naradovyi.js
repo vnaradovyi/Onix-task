a79594449e63418134ddc57866abf08bd9357a8c
Volodymyr Naradovyi <103950693+vnaradovyi@users.noreply.github.com>
2022-10-04 10:29:42 +0300

    Update naradovyi.js

--- naradovyi.js
+++ naradovyi.js
@@ -4,27 +4,54 @@ const axios = require('axios');
 
 const PORT = 3000;
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer((userRequest, serverResponse) => { 
     
-    const CURRENCY = url.parse(req.url, true).query.currency;
+    const USER_PATH = url.parse(userRequest.url, true);
 
-    axios
-        .get('https://api.coincap.io/v2/assets/'+ CURRENCY)
-        .then(result => {
+    if (USER_PATH.pathname == '/rates') {
 
-            res.writeHead(result.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({
-                usd: `${result.data.data.priceUsd}`
+        const CURRENCY = USER_PATH.query.currency;
+
+        if (CURRENCY != undefined) {
+
+            axios
+            .get('https://api.coincap.io/v2/assets/'+ CURRENCY)
+            .then(result => {
+
+                serverResponse.writeHead(result.status, { 'Content-Type': 'application/json' });
+
+                serverResponse.end(JSON.stringify({
+                    usd: `${result.data.data.priceUsd}`
+                }));
+
+            }) 
+            .catch(error => {
+
+                serverResponse.writeHead(404, { 'Content-Type': 'application/json' });
+                    
+                serverResponse.end(JSON.stringify({
+                    'Error': `${error.message}: Invalid name of crypto currency. See https://docs.coincap.io/`
+                }));
+            });
+
+        } else {
+
+            serverResponse.writeHead(404, { 'Content-Type': 'application/json' });
+                
+            serverResponse.end(JSON.stringify({
+                'Invalid key of search': 'Must be: /rates?currency=cur_name, where cur_name is the name of crypto currency (bitcoin or other)',            
             }));
 
-        })
-        .catch(error => {
+        }
 
-            res.writeHead(404, { 'Content-Type': 'text' });
-            res.end(error.message);
+    } else {
 
-        });
-    
+        serverResponse.writeHead(404, { 'Content-Type': 'application/json' });
+                
+        serverResponse.end(JSON.stringify({
+            'Invalid path': 'Must be: http://localhost:3000/rates?currency=cur_name, where cur_name is the name of crypto currency (bitcoin or other)',            
+        }));
+    }
 });
 
 server.listen(PORT);
